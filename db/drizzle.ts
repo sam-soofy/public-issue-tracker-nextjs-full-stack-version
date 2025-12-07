b245336c66befb4395b9d06e509aef7b859e58bb
sam-soofy <mj_1997_2022@proton.me>
2025-12-07 09:08:49 +0330

    feat: Setup Drizzle

---
+++ db/drizzle.ts
@@ -0,0 +1,11 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+
+// You can specify any property from the node-postgres connection options
+const db = drizzle({
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  },
+});
+
+const result = await db.execute("select 1");
